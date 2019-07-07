09314f693925624b13fc30f6848cbfc516d5a75c
hongri <1285052668@qq.com>
2019-07-07 17:52:43 +0800

    FrameLayout两层View事件传递的问题--如果希望点击上层View的区域的时候，将事件传递至下层View，那么可以将上层View的clickable状态至为false

---
+++ app/src/main/java/com/xiaoqlu/motionevent/FrameLayoutActivity.java
@@ -0,0 +1,51 @@
+package com.xiaoqlu.motionevent;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import com.xiaoqlu.motionevent.util.Logger;
+
+/**
+ * FrameLayout相关事件的透传
+ *
+ * FrameLayout上下两层View分别是A和B，如何实现点击A的时候，让B触发事件：
+ * 将View A设置：tvA.setClickable(false);
+ */
+public class FrameLayoutActivity extends Activity {
+
+    private Button btnA, btnB;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layout);
+
+        btnA = (Button)findViewById(R.id.btnA);
+        btnB = (Button)findViewById(R.id.btnB);
+
+        //方法1
+        btnA.setClickable(false);
+        btnA.setFocusable(false);
+
+        //方法2
+        //btnA.setOnTouchListener(new OnTouchListener() {
+        //    @Override
+        //    public boolean onTouch(View v, MotionEvent event) {
+        //        return true;
+        //    }
+        //});
+
+
+        btnB.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Logger.d("tvB###onClick");
+            }
+        });
+
+    }
+}
